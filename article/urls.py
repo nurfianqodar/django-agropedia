4efa35d848735e07a53462f40322136b5c277ad9
Nurfian Qodar <77nurfianqodar@gmail.com>
2024-03-01 03:39:27 +0700

    nope

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.ListArticleView.as_view()),
-    path('categories/', views.ArticleCategoryListView.as_view()),
-    path('categories/<str:category>/', views.ArticleCategoryView.as_view()),
-    path('article/<str:slug>/', views.ArticleView.as_view())
+    path("", views.ListArticleView.as_view()),
+    path("categories/", views.ArticleCategoryListView.as_view()),
+    path("categories/<str:category>/", views.ArticleCategoryView.as_view()),
+    path("article/<str:slug>/", views.ArticleView.as_view()),
 ]
